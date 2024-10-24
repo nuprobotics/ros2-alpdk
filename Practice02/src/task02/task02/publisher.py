dd44570e52301bd326c5660b1aef7a560a6a5b5a
alpdk <alexander@podkopaev.net>
2024-10-24 13:24:34 +0300

    Change publisher init

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -4,12 +4,12 @@ from std_msgs.msg import String
 from rclpy.parameter import Parameter
 
 class Publisher(Node):
-    def __init__(self):
+    def __init__(self, text = 'Hello, ROS2!'):
         super().__init__('publisher')
 
         # Load parameters from the config file
         self.declare_parameter('topic_name', '/spgc/receiver')
-        self.declare_parameter('text', 'Hello, ROS2!')
+        self.declare_parameter('text', text)
 
         self.topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
         self.text = self.get_parameter('text').get_parameter_value().string_value
@@ -26,16 +26,12 @@ class Publisher(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = Publisher()
 
     # Read command line parameter
     if len(args) > 1:
-        try:
-            # Overriding the default message if provided
-            node.text = args[1]
-            node.set_parameters([Parameter('text', value=node.text)])
-        except Exception as e:
-            node.get_logger().error(f'Error setting text parameter: {e}')
+        node = Publisher(args[1])
+    else:
+        node = Publisher()
 
     rclpy.spin(node)
     node.destroy_node()
